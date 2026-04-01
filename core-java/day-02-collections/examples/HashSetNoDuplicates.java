0b63ffc2ad48af3bb7e5c8abfe4572c513f984f7
guruvishnu <guruvishnu1927@gmail.com>
2026-04-02 00:28:09 +0530

    Day 2: Collections deep dive - ArrayList, HashSet, HashMap with real examples

---
+++ core-java/day-02-collections/examples/HashSetNoDuplicates.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+import java.util.Set;
+
+
+class HashSetNoDuplicates{
+    public static void main(String[] args) {
+        Set<Integer> numbers = new HashSet();
+        numbers.add(10);
+        numbers.add(30);
+        numbers.add(10);
+
+        System.out.println("numbers: " + numbers);
+    }
+}
